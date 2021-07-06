7cb71911fc3d4a5aef5cb552f0459f6c64fe3bed
sotolf2 <56679258+sotolf2@users.noreply.github.com>
2021-07-06 15:21:38 +0200

    Fixed some bugs in tests
    I was writing directly into memory in one of my tests, but that doesn't work anymore after the refactoring that I did
    Also fixed some programs that didn't end with a brk op.

--- tests/cpu/test_dec.nim
+++ tests/cpu/test_dec.nim
@@ -3,7 +3,7 @@ import ../../cpu.nim
 
 # test 0xa9 lda immedeate load data
 var t_cpu = newCpu()
-let test_program = "a9 01 8d 10 00 ce 10 00 ce 10 00 ad 10 00"
+let test_program = "a9 01 8d 10 00 ce 10 00 ce 10 00 ad 10 00 00"
 
 t_cpu.load_and_run(test_program)
 
--- tests/cpu/test_lda_from_memory.nim
+++ tests/cpu/test_lda_from_memory.nim
@@ -3,8 +3,7 @@ import ../../cpu.nim
 
 # test 0xa9 lda immedeate load data
 var t_cpu = newCpu()
-t_cpu.mem_write(0x10'u16, 0x55'u8)
-let test_program = "a5 10 00"
+let test_program = "a9 55 85 10 a9 00 a5 10 00"
 
 t_cpu.load_and_run(test_program)
 
